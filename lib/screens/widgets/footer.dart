638587a4e188ff615086e1b1f83f64fc1e9ef24e
frendy app <frendyapp@gmail.com>
2020-11-29 01:18:02 -0800

    'ddd'

--- lib/screens/widgets/footer.dart
+++ lib/screens/widgets/footer.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ccrwork_mobile/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,15 +9,26 @@ class Footer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         color: Colors.white,
-        child: CarouselSlider(
-          options: CarouselOptions(autoPlay: true),
-          items: imgList
-              .map((item) => Container(
-                    child: Center(
+        child: Column(children: [
+          Container(
+            width: double.infinity,
+            height: 50,
+            color: AppColor.mainColor,
+            child: Center(child: Text('Clients We Work With')),
+          ),
+          CarouselSlider(
+            options: CarouselOptions(autoPlay: true, height: 100),
+            items: imgList
+                .map((item) => Container(
+                      child: Center(
                         child: Image.asset(
-                            'assets/images/comp_${item.toString()}.jpg')),
-                  ))
-              .toList(),
-        ));
+                          'assets/images/comp_${item.toString()}.jpg',
+                          height: 100,
+                        ),
+                      ),
+                    ))
+                .toList(),
+          )
+        ]));
   }
 }
